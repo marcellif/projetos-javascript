ea456604f30434bb270be10473289fde88bbc0aa
marcelli <marcelli.faria@gmail.com>
2025-09-29 21:52:05 -0300

    commitando while

---
+++ enquanto.js
@@ -0,0 +1,13 @@
+let contador = 0
+while (contador <= 10) {
+    console.log('Número atual:', contador);
+    contador++;
+}
+
+
+
+let numero = 1
+do {
+    console.log(numero)
+    numero ++
+}while (numero <= 10)
