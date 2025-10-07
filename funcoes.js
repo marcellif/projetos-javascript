6bb7fae277e6638c5a98690eeda71fc7b8576ceb
marcelli <marcelli.faria@gmail.com>
2025-10-07 19:34:32 -0300

    subindo modificações arquivo de funções

--- funcoes.js
+++ funcoes.js
@@ -6,9 +6,16 @@
 
 
  function quadrado(numero){
-    let aoQuadrado = 0
-    aoQuadrado = numero * numero
+    let aoQuadrado = numero * numero
     console.log(aoQuadrado)
  }
 
-quadrado(50)
+//quadrado(50)
+
+function calculaquadrado(num){
+   return num ** 2
+ }
+
+const numeroDobrado  = calculaquadrado(50)
+
+console.log(`O quadrado é: ${numeroDobrado}` )
