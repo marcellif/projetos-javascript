a58a979687a5a93072051d6a5b4a9de1ca9ed211
marcelli <marcelli.faria@gmail.com>
2025-10-07 19:59:50 -0300

    refatorando caso função dobrar numero com arrow function simplificada

--- arrowSimplificada.js
+++ arrowSimplificada.js
@@ -2,3 +2,7 @@ const comprimento = cliente => console.log (`seja bem vindo(a), ${cliente}, à n
 
 comprimento ("Perciliana")
 
+
+const calcularDobroNum = numero => numero * 2
+
+console.log(calcularDobroNum (1000))
