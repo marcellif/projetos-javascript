37c837bf315e005adf22ad9da27043af0ca5e749
marcelli <marcelli.faria@gmail.com>
2025-10-07 19:51:03 -0300

    funções em arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,12 @@
+const saudacao  = (nome) => {
+    console.log(`vida longa e próspera, ${nome}`)
+}
+
+saudacao("Helio")
+
+
+const numeroDobrado = (num) => {
+   return num * 2
+}
+
+console.log(numeroDobrado(8))
