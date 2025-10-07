6c4202a2a0b6b07dbfea1663078c090a0763d471
marcelli <marcelli.faria@gmail.com>
2025-10-07 19:30:21 -0300

    funções de exemplo

---
+++ funcoes.js
@@ -0,0 +1,14 @@
+ function saudacao (nome){
+    console.log (`olá ${nome}`)
+ }
+
+ //saudacao("mariana")
+
+
+ function quadrado(numero){
+    let aoQuadrado = 0
+    aoQuadrado = numero * numero
+    console.log(aoQuadrado)
+ }
+
+quadrado(50)
