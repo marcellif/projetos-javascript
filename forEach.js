0d54140d5b3d6013759d7a80cc5e6405f8c7f7f8
marcelli <marcelli.faria@gmail.com>
2025-10-15 19:14:28 -0300

    for each e for of

---
+++ forEach.js
@@ -0,0 +1,14 @@
+let coresNaturais = [
+    "vermelho",
+    "verde",
+    "amarelo",
+    "laranja",
+    "roxo",
+    "azul" 
+  ];
+
+  console.log("as cores naturais são")
+
+  coresNaturais.forEach((valor, indice) =>{
+    console.log(`No valor ${indice}, a cor é ${valor} `)
+  })
