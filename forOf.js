9279fcfb3afa73d3182df5598db1bc83cfbf728d
marcelli <marcelli.faria@gmail.com>
2025-10-15 19:00:39 -0300

    for each

---
+++ forOf.js
@@ -0,0 +1,6 @@
+let tarefasDoDia = ['acordar', 'tomar banho',  'pegar transporte', 'fazer exercícios', 'preparar jantar', 'ler um livro', 'lavar louça', 'dormir'];
+
+
+for ( let tarefa of tarefasDoDia){
+    console.log(`Lembrar de: ${tarefa} `)
+}
