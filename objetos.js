42b25030b1c1e09e8534f212859e7a732d55023e
marcelli <marcelli.faria@gmail.com>
2025-10-16 18:05:50 -0300

    objetos inicio

---
+++ objetos.js
@@ -0,0 +1,21 @@
+const pessoa ={
+    nome: 'Paula',
+    idade: 25,
+    temCnh: true
+
+}
+
+pessoa.sobrenome = 'Freitas'
+
+console.log('Nome: ', pessoa.nome);
+console.log('Sobrenome: ', pessoa.sobrenome);
+
+
+const livro = {
+    titulo: 'O Hobbit',
+    autor: 'J. R. R. Tolkien',
+    paginas: 310
+};
+
+livro.publicado = true;
+livro.idiomas = ['Inglês', 'Português', 'Espanhol'];
