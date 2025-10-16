b9ee35575c2688910a003ce36107cb615e01c2da
marcelli <marcelli.faria@gmail.com>
2025-10-16 18:19:19 -0300

    subindo interaç~eos com objetos

--- objetos.js
+++ objetos.js
@@ -19,3 +19,27 @@ const livro = {
 
 livro.publicado = true;
 livro.idiomas = ['Inglês', 'Português', 'Espanhol'];
+
+livro.idiomas.push('Mandarim')
+livro.idiomas.push('Francês')
+
+console.log('Livro antes:', livro)
+delete livro.paginas
+console.log('Livro depois:', livro)
+
+
+//Outra funcionalidade interessante é a forma de acessar propriedades. 
+//Podemos fazer isso como livro.idiomas, 
+//mas também podemos usar colchetes e strings para acessar, por exemplo, o autor.
+console.log('Autor do livro', livro['autor'])
+
+const autor = {
+    nome: 'J R R Tolkien',
+    nacionalidade: 'Britanico',
+    idade: 98
+}
+
+
+livro.autor = autor
+
+console.log('Livro final', livro)
